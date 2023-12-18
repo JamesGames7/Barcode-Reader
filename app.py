0ebae171c9c5a92102577dade39ba13a51f1a24a
JamesGames7 <139825533+JamesGames7@users.noreply.github.com>
2023-12-18 11:59:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/read/')
+def read():
+    return render_template('read.html')
